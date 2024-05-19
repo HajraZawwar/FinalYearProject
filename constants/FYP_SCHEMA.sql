db88f9b6d7109b33941685fec78c3d5b514fad69
AsadNazir <nazirgill1054@gmail.com>
2024-05-19 21:35:49 +0500

    schema changed

--- constants/FYP_SCHEMA.sql
+++ constants/FYP_SCHEMA.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `fyp` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `fyp`;
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: localhost    Database: fyp
@@ -100,24 +98,27 @@ DROP TABLE IF EXISTS `courseoffering`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `courseoffering` (
-  `courseOfferingID` int NOT NULL,
+  `courseOfferingID` int NOT NULL AUTO_INCREMENT,
   `department` int NOT NULL,
   `session` int NOT NULL,
   `batch` int NOT NULL,
   `section` int NOT NULL,
   `campus` int NOT NULL,
+  `course` int NOT NULL,
   PRIMARY KEY (`courseOfferingID`),
   KEY `department_idx` (`department`),
   KEY `batch_idx` (`batch`),
   KEY `session_idx` (`session`),
   KEY `section_idx` (`section`),
   KEY `campus_idx` (`campus`),
+  KEY `course_idx` (`course`),
   CONSTRAINT `batch` FOREIGN KEY (`batch`) REFERENCES `batch` (`BatchID`),
   CONSTRAINT `campus` FOREIGN KEY (`campus`) REFERENCES `campus` (`CampusID`),
+  CONSTRAINT `course` FOREIGN KEY (`course`) REFERENCES `course` (`CourseID`),
   CONSTRAINT `department` FOREIGN KEY (`department`) REFERENCES `department` (`DepartmentID`),
   CONSTRAINT `section` FOREIGN KEY (`section`) REFERENCES `section` (`SectionID`),
   CONSTRAINT `session` FOREIGN KEY (`session`) REFERENCES `session` (`SessionID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -126,6 +127,7 @@ CREATE TABLE `courseoffering` (
 
 LOCK TABLES `courseoffering` WRITE;
 /*!40000 ALTER TABLE `courseoffering` DISABLE KEYS */;
+INSERT INTO `courseoffering` VALUES (1,1,1,1,2,1,2);
 /*!40000 ALTER TABLE `courseoffering` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -147,8 +149,8 @@ CREATE TABLE `courseregistration` (
   PRIMARY KEY (`CourseRegistrationID`),
   KEY `SemesterRegistrationID` (`SemesterRegistrationID`),
   KEY `TeacherID` (`TeacherID`),
-  KEY `courseofferingID_idx` (`CourseOfferingID`),
-  CONSTRAINT `courseofferingID` FOREIGN KEY (`CourseOfferingID`) REFERENCES `courseoffering` (`courseOfferingID`),
+  KEY `courseoffering_idx` (`CourseOfferingID`),
+  CONSTRAINT `courseoffering` FOREIGN KEY (`CourseOfferingID`) REFERENCES `courseoffering` (`courseOfferingID`),
   CONSTRAINT `courseregistration_ibfk_1` FOREIGN KEY (`SemesterRegistrationID`) REFERENCES `semesterregistration` (`SemesterRegistrationID`),
   CONSTRAINT `courseregistration_ibfk_4` FOREIGN KEY (`TeacherID`) REFERENCES `teachers` (`TeacherID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -259,7 +261,7 @@ CREATE TABLE `login` (
   PRIMARY KEY (`loginId`),
   KEY `role_idx` (`role`),
   CONSTRAINT `role` FOREIGN KEY (`role`) REFERENCES `roles` (`roleId`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -268,7 +270,7 @@ CREATE TABLE `login` (
 
 LOCK TABLES `login` WRITE;
 /*!40000 ALTER TABLE `login` DISABLE KEYS */;
-INSERT INTO `login` VALUES (2,'asad','$2b$10$yv2Da8aYwMxPx1OuhTs8C.pmz92NOpGmNzUdyMyNE0tfTSG6ZLUwq',1),(3,'asad4','$2b$10$v6onFyc03Whi6LbZnCcaLu2KB8bV2GzaNF4STL2bP.ZMuz6G573dS',1),(4,'asad890','$2b$10$Ek0fQmux7AeWj3v.V/c5TOb79ILtmm6aLq3I2OJUqFH1GaAxDeVzm',1);
+INSERT INTO `login` VALUES (2,'asad','$2b$10$yv2Da8aYwMxPx1OuhTs8C.pmz92NOpGmNzUdyMyNE0tfTSG6ZLUwq',1),(3,'asad4','$2b$10$v6onFyc03Whi6LbZnCcaLu2KB8bV2GzaNF4STL2bP.ZMuz6G573dS',1),(4,'asad890','$2b$10$Ek0fQmux7AeWj3v.V/c5TOb79ILtmm6aLq3I2OJUqFH1GaAxDeVzm',1),(7,'2024007','$2b$10$Nx1Zcjs6942SuHElB0lu1erJ9d/dBycsxZIsYKuJVk88mbUARZ1Ha',1),(8,'98998898','$2b$10$oC6GhkvxTu.y8axrlLZWrunxIwaOutqSnOAkvGS8MIXtxpj7k9cWi',1),(9,'2024083','$2b$10$HFJrbdzXjTaHGSWjmFgLy.0UWk/YMXnnNGvnilMTTSmAs2i0AokIu',1);
 /*!40000 ALTER TABLE `login` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -312,7 +314,7 @@ CREATE TABLE `roles` (
   `roleId` int NOT NULL AUTO_INCREMENT,
   `role` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`roleId`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -321,7 +323,7 @@ CREATE TABLE `roles` (
 
 LOCK TABLES `roles` WRITE;
 /*!40000 ALTER TABLE `roles` DISABLE KEYS */;
-INSERT INTO `roles` VALUES (1,'student');
+INSERT INTO `roles` VALUES (1,'studentzzzzzzzz'),(2,'teacher');
 /*!40000 ALTER TABLE `roles` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -336,7 +338,7 @@ CREATE TABLE `section` (
   `SectionID` int NOT NULL AUTO_INCREMENT,
   `SectionName` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`SectionID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -345,7 +347,7 @@ CREATE TABLE `section` (
 
 LOCK TABLES `section` WRITE;
 /*!40000 ALTER TABLE `section` DISABLE KEYS */;
-INSERT INTO `section` VALUES (1,'Morning'),(2,'Evening');
+INSERT INTO `section` VALUES (1,'Morning'),(2,'Afternoon'),(3,'Dopehr');
 /*!40000 ALTER TABLE `section` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -361,13 +363,15 @@ CREATE TABLE `semesterregistration` (
   `StudentID` int NOT NULL,
   `SemesterNumber` int NOT NULL,
   `SessionID` int NOT NULL,
-  `status` varchar(45) DEFAULT NULL,
+  `status` int NOT NULL,
   PRIMARY KEY (`SemesterRegistrationID`),
   UNIQUE KEY `StudentID_UNIQUE` (`StudentID`),
   KEY `SessionID` (`SessionID`),
+  KEY `status_idx` (`status`),
   CONSTRAINT `semesterregistration_ibfk_1` FOREIGN KEY (`StudentID`) REFERENCES `students` (`StudentID`),
-  CONSTRAINT `semesterregistration_ibfk_2` FOREIGN KEY (`SessionID`) REFERENCES `session` (`SessionID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `semesterregistration_ibfk_2` FOREIGN KEY (`SessionID`) REFERENCES `session` (`SessionID`),
+  CONSTRAINT `status` FOREIGN KEY (`status`) REFERENCES `statuses` (`statusID`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -376,6 +380,7 @@ CREATE TABLE `semesterregistration` (
 
 LOCK TABLES `semesterregistration` WRITE;
 /*!40000 ALTER TABLE `semesterregistration` DISABLE KEYS */;
+INSERT INTO `semesterregistration` VALUES (4,6,5,1,1),(6,9,5,1,1),(7,10,2,1,1);
 /*!40000 ALTER TABLE `semesterregistration` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -403,6 +408,31 @@ LOCK TABLES `session` WRITE;
 /*!40000 ALTER TABLE `session` DISABLE KEYS */;
 INSERT INTO `session` VALUES (1,'10','2024-05-09','2025-08-09');
 /*!40000 ALTER TABLE `session` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `statuses`
+--
+
+DROP TABLE IF EXISTS `statuses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `statuses` (
+  `statusID` int NOT NULL AUTO_INCREMENT,
+  `statusName` varchar(45) NOT NULL,
+  PRIMARY KEY (`statusID`),
+  UNIQUE KEY `statusName_UNIQUE` (`statusName`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `statuses`
+--
+
+LOCK TABLES `statuses` WRITE;
+/*!40000 ALTER TABLE `statuses` DISABLE KEYS */;
+INSERT INTO `statuses` VALUES (1,'active'),(3,'frozen'),(2,'suspended');
+/*!40000 ALTER TABLE `statuses` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -441,7 +471,7 @@ CREATE TABLE `students` (
   CONSTRAINT `DepartmentID` FOREIGN KEY (`DepartmentID`) REFERENCES `department` (`DepartmentID`),
   CONSTRAINT `login` FOREIGN KEY (`login`) REFERENCES `login` (`loginId`),
   CONSTRAINT `SectionID` FOREIGN KEY (`SectionID`) REFERENCES `section` (`SectionID`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -450,8 +480,37 @@ CREATE TABLE `students` (
 
 LOCK TABLES `students` WRITE;
 /*!40000 ALTER TABLE `students` DISABLE KEYS */;
-INSERT INTO `students` VALUES (6,'2024007','John','Doe',20,'Male','New York','USA','123-456-7890','123 Main St',1,1,1,1,'active',NULL);
+INSERT INTO `students` VALUES (6,'2024007','Jhooonnn','Snooww',20,'Male','New York','USA','123-456-7890','123 Main St',1,1,1,1,'active',7),(9,'98998898','John','Doe',20,'Male','New York','USA','123-456-7890','123 Main St',1,1,1,1,'active',8),(10,'2024083','Jhooonnn','Snooww',20,'Male','New York','USA','123-456-7890','123 Main St',1,1,1,1,'active',9);
 /*!40000 ALTER TABLE `students` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `supervisor`
+--
+
+DROP TABLE IF EXISTS `supervisor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `supervisor` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `teacherId` int DEFAULT NULL,
+  `departmentId` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `teacherId_idx` (`teacherId`),
+  KEY `departmentId_idx` (`departmentId`),
+  CONSTRAINT `deptId` FOREIGN KEY (`departmentId`) REFERENCES `department` (`DepartmentID`),
+  CONSTRAINT `teacherId` FOREIGN KEY (`teacherId`) REFERENCES `teachers` (`TeacherID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `supervisor`
+--
+
+LOCK TABLES `supervisor` WRITE;
+/*!40000 ALTER TABLE `supervisor` DISABLE KEYS */;
+INSERT INTO `supervisor` VALUES (1,1,2);
+/*!40000 ALTER TABLE `supervisor` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -471,10 +530,12 @@ CREATE TABLE `teachers` (
   `PhoneNo` varchar(20) DEFAULT NULL,
   `Email` varchar(255) DEFAULT NULL,
   `DepartmentID` int DEFAULT NULL,
+  `login` int DEFAULT NULL,
   PRIMARY KEY (`TeacherID`),
+  UNIQUE KEY `TeacherCode_UNIQUE` (`TeacherCode`),
   KEY `DepartmentID` (`DepartmentID`),
   CONSTRAINT `teachers_ibfk_1` FOREIGN KEY (`DepartmentID`) REFERENCES `department` (`DepartmentID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -483,6 +544,7 @@ CREATE TABLE `teachers` (
 
 LOCK TABLES `teachers` WRITE;
 /*!40000 ALTER TABLE `teachers` DISABLE KEYS */;
+INSERT INTO `teachers` VALUES (1,'TCH123','John','Doe',35,'Male','+1234567890','john.doe@example.com',1,NULL),(3,'TCH126','John','Doe',35,'Male','+1234567890','john.doe@example.com',1,NULL);
 /*!40000 ALTER TABLE `teachers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -531,4 +593,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-13  1:46:36
+-- Dump completed on 2024-05-19 21:34:18
